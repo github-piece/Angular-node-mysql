fb7c9143a97f764060867d811e302ef46c0e04b8
jinri-coding <puchenglie@gmail.com>
2020-03-28 03:54:38 +0800

    header striky

--- src/app/includes/front/header/header.component.ts
+++ src/app/includes/front/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isSticky = false;
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.isSticky = window.pageYOffset >= 250;
+  }
 }
